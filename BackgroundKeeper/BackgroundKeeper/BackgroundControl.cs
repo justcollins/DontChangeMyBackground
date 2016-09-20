bac0ea99077e2e10f805b0eb8d09658bfed17ce9
Justin Collins <justcollins@comcast.net>
2016-09-20 16:30:19 -0700

    Initial Pull
    The background keeper is able to continuiously place an image as the
    background if it detects a change. Currently this is all it does. I
    would like to make sure that it only does this operation once in future
    development. As of now, im happy with it as it will get done what i want
    it to do.

---
+++ BackgroundKeeper/BackgroundKeeper/BackgroundControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BackgroundKeeper {
+    public class BackgroundControl {
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+
+        private static readonly UInt32 SPI_SETDESKWALLPAPER = 0x14;
+        private static readonly UInt32 SPIF_UPDATEINIFILE = 0x01;
+        private static readonly UInt32 SPIF_SENDWININICHANGE = 0x02;
+        private static readonly UInt32 WM_SETTINGCHANGE = 0x1A;
+        private static string path;
+
+        public BackgroundControl(string _path) {
+            path = _path;
+        }
+
+        public string getPath() {
+            return path;
+        }
+
+        public UInt32 CheckChange() {
+            return WM_SETTINGCHANGE;
+        }
+
+        public UInt32 CheckSetWallPaper() {
+            return SPI_SETDESKWALLPAPER;
+        }
+
+        public void SetWallpaper(String path) {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
---
+++ BackgroundKeeper/BackgroundKeeper/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace BackgroundKeeper {
+    public partial class Form1 : Form {
+
+        private BackgroundControl background = new BackgroundControl(@"C:\Users\Intern\Pictures\WallPaper\StarWars.jpg");
+        private bool onlyDoOnce = false;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            this.Hide();
+            NotifyIcon trayIcon = new NotifyIcon();
+            trayIcon.Icon = new Icon(@"D:\GitHub\DontChangeMyBackground\BackgroundKeeper\BackgroundKeeper\Icons\AssetStoreFiddler_icon.ico");
+            trayIcon.Visible = true;
+        }
+
+        protected override void WndProc(ref Message message) {
+            if (message.Msg == background.CheckChange()) {
+                if (message.WParam.ToInt32() == background.CheckSetWallPaper()) {
+                    background.SetWallpaper(background.getPath());
+                    //Process.Start("http://www.google.com/");
+                }
+            }
+            base.WndProc(ref message);
+        }
+    }
+}
